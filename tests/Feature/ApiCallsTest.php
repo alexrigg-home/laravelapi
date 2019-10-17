ea87e0e37332cc7b7fc869c60f6ab8ccefecce44
alex <alex@alexrigg.com>
2019-10-17 05:57:31 -0700

    updated tests

--- tests/Feature/ApiCallsTest.php
+++ tests/Feature/ApiCallsTest.php
@@ -5,11 +5,14 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use App\User;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ApiCallsTest extends TestCase
 {
+    //refreshes db then uses transactions to clear data between tests
+    use RefreshDatabase;
     /**
      * login test
      *
@@ -56,26 +59,35 @@ class ApiCallsTest extends TestCase
      *
      * @return void
      */
-    public function testArticles()
+    public function testAllArticles()
     {
         $testpass = encryptpassword('password123');
         $user = factory('App\User')->create([
-                'name' => 'testbob2',
-                'email' => 'bob2@testing.com', 
+                'name' => 'testbob',
+                'email' => 'bob@testing.com', 
                 'password' => $testpass,
                 'api_token' => 'exampleapitoken'
         ]);
 
         $article = factory('App\Article')->create([
-                'title' => 'my example article title',
-                'body' => 'My example article desciption for testing purposes'
+                'title' => 'my first example article title',
+                'body' => 'My first example article description for testing purposes'
         ]);
-                
-        
+
+        $article = factory('App\Article')->create([
+                'title' => 'my second example article title',
+                'body' => 'My second example article description for testing purposes'
+        ]);
+
+        $article = factory('App\Article')->create([
+                'title' => 'my third example article title',
+                'body' => 'My third example article description for testing purposes'
+        ]);
+                        
         $response = $this->withHeaders([
             'Content-Type' => 'application/json',
             'Accept' => 'application/json',
-            'Authorization'=> 'exampleapitoken'
+            'Authorization'=> 'Bearer exampleapitoken'
         ])->json(
             'GET', 
             '/api/articles'
@@ -84,8 +96,59 @@ class ApiCallsTest extends TestCase
          $response
             ->assertStatus(200)
             ->assertJsonFragment([
-                'title' => 'my example article title',
-                'body' => 'My example article desciption for testing purposes'               
+                'title' => 'my first example article title'
+            ]); 
+        $response
+            ->assertStatus(200)
+            ->assertJsonFragment([
+                'title' => 'my second example article title'
+            ]); 
+        $response
+            ->assertStatus(200)
+            ->assertJsonFragment([
+                'body' => 'My third example article description for testing purposes'
             ]); 
     }
+
+    /**
+     *  test get articles
+     *
+     * @return void
+     */
+    public function testSingleArticle()
+    {
+
+        $testpass = encryptpassword('password123');
+        $user = factory('App\User')->create([
+                'name' => 'testbob',
+                'email' => 'bob@testing.com', 
+                'password' => $testpass,
+                'api_token' => 'exampleapitoken'
+        ]);
+
+        $article = factory('App\Article')->create([
+                'title' => 'my example article title',
+                'body' => 'My example article desciption for testing purposes'
+        ]);
+                        
+        //annoyingly the refresh db doesnt refresh autoincrement!
+        $response = $this->withHeaders([
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json',
+            'Authorization'=> 'Bearer exampleapitoken'
+        ])->json(
+            'GET', 
+            '/api/articles/4'
+         );
+             
+
+        $response
+            ->assertStatus(200)
+            ->assertJsonFragment([
+                'title' => 'my example article title',
+                'body' =>  'My example article desciption for testing purposes'
+            ]); 
+    }
+
+
 }
